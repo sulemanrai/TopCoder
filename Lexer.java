24f65c3e173e65859080f3c855bb7ed3250b044f
sulemanrai <suleman.rai@gmail.com>
2017-04-09 18:11:53 +0800

    Lexer

---
+++ Lexer.java
@@ -0,0 +1,70 @@
+public class Lexer {
+	int number_consumed = 0;
+	String[] stringArrayconsumed;
+
+	public String[] tokenize(String[] tokens, String input) {
+		stringArrayconsumed = new String[input.length()];
+		String[] consumed = {};
+		String[] sortedtokens = {};
+		if (tokens.equals(null)) {
+			return stringArrayconsumed;
+		} else {
+			sortedtokens = SortAndFindMaxToken(tokens);
+			while (input.length() != 0) {				
+				input = check_if_token_exists(sortedtokens, input);
+				if (input.length() != 0) {
+					input = input.substring(1);
+				}
+			}
+
+			consumed = new String[number_consumed];			
+			for (int i = 0; i < number_consumed; i++) {
+				consumed[i] = stringArrayconsumed[i];
+			}
+			return consumed;
+		}
+	}
+
+	public String[] SortAndFindMaxToken(String[] tokens) {
+		String[] sortedtokens = {};
+		for (int i = (tokens.length - 1); i >= 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				if (tokens[j - 1].length() < tokens[j].length()) {
+					String temp = tokens[j - 1];
+					tokens[j - 1] = tokens[j];
+					tokens[j] = temp;
+				}
+			}
+		}
+
+		sortedtokens = tokens;		
+		return sortedtokens;
+	}
+
+
+	String check_if_token_exists(String[] tokens, String input) {
+		boolean token_exists = false;
+		String output = "";
+		while (input.length() != 0) {
+			for (int i = 0; i < tokens.length; i++) {
+				if (input.length() >= tokens[i].length()) {
+					token_exists = (input.substring(0, tokens[i].length()).equals(tokens[i]));
+					if (token_exists == true) {
+						stringArrayconsumed[number_consumed] = tokens[i];
+						number_consumed++;
+						input = input.substring(tokens[i].length());
+						output = input;						
+						break;
+					} else {
+						output = input;
+					}
+
+				}
+			}
+			if (token_exists == false) {
+				break;
+			}
+		}
+		return output;
+	}
+}
